94fd5fde958d38588cb63cbe409148305ab96e62
Walcygleicson <walcygleicsonoliveira@gmail.com>
2023-10-18 16:35:55 -0300

    Main Content

--- Scripts/main.js
+++ Scripts/main.js
@@ -1,12 +1,17 @@
 import Header from "./Modules/Header.js";
 import Footer from "./Modules/Footer.js";
-import { connectKey } from "./Modules/aux-tools.js";
+import { connectKey, $ } from "./Modules/aux-tools.js";
+import svg from "./Modules/svg-icons.js";
 
 
 
 connectKey.create()
 Header('#header-capsule')
 Footer('#footer-capsule')
+
+$('.rank-svg-capsule').innerHTML = svg.rank()
+
+
 
 
 
