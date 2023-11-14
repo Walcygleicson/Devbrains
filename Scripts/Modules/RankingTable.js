42aa9aa151f129d1c10d69204235dc25c748067b
Walcygleicson <walcygleicsonoliveira@gmail.com>
2023-11-14 10:22:10 -0300

    Last Update

--- Scripts/Modules/RankingTable.js
+++ Scripts/Modules/RankingTable.js
@@ -33,60 +33,64 @@ export default function RankingTable(lang) {
 
     // Ontem infos do usuário ou de noLoginUser
     const userRanking = connectKey.get('user') == null ? noLoginUser.get('ranking') : getUser().ranking
+
+    if (Object.keys(userRanking).length > 0) {
+        console.log(Object.keys(userRanking))
+        
+        userRanking[infos.lang].forEach((round, i) => {
+            const row = '.dr' + i
     
-    userRanking[infos.lang].forEach((round, i) => {
-        const row = '.dr' + i
-
-        //TABLE DE PARTIDA ATUAL
-        if (round.current) {
+            //TABLE DE PARTIDA ATUAL
+            if (round.current) {
+                // Posição do Rank
+                $('.d-row .rank-number').textContent = '#' + (i + 1)
+                // Level
+                $('.d-row .level').textContent = captalize(round.level)
+                // Pontuação
+                $('.d-row .score').textContent = floatFormat(round.score, 1) + ' pts'
+                // Número de respostas corretas
+                $('.d-row .correct-number').textContent = round.correctNumb
+                // Número de respostas erradas
+                $('.d-row .errors-number').textContent = round.errorNumb
+                // Data e Hora
+                $('.d-row .date-hours').textContent = round.data + ' - ' + round.hora 
+            }
+    
+            //Estilização do TOP 3 Ranking
+            //Insere ID
+            switch (i) {
+                case 0:
+                    $(row).id = 'first'
+                    break
+                case 1:
+                    $(row).id = 'second'
+                    break
+                case 2:
+                    $(row).id = 'third'
+                    break
+            }
+    
+            //Retirar rolagem Y do html
+            $('html').style.overflowY = "hidden"
+    
+    
+    
+    
+            //TABELA DE RANKING
             // Posição do Rank
-            $('.d-row .rank-number').textContent = '#' + (i + 1)
+            $(row + ' .rank-number').textContent = '#' + (i + 1)
             // Level
-            $('.d-row .level').textContent = captalize(round.level)
+            $(row + ' .level').textContent = captalize(round.level)
             // Pontuação
-            $('.d-row .score').textContent = floatFormat(round.score, 1) + ' pts'
+            $(row + ' .score').textContent = floatFormat(round.score, 1) + ' pts'
             // Número de respostas corretas
-            $('.d-row .correct-number').textContent = round.correctNumb
+            $(row + ' .correct-number').textContent = round.correctNumb
             // Número de respostas erradas
-            $('.d-row .errors-number').textContent = round.errorNumb
+            $(row + ' .error-number').textContent = round.errorNumb
             // Data e Hora
-            $('.d-row .date-hours').textContent = round.data + ' - ' + round.hora 
-        }
-
-        //Estilização do TOP 3 Ranking
-        //Insere ID
-        switch (i) {
-            case 0:
-                $(row).id = 'first'
-                break
-            case 1:
-                $(row).id = 'second'
-                break
-            case 2:
-                $(row).id = 'third'
-                break
-        }
-
-        //Retirar rolagem Y do html
-        $('html').style.overflowY = "hidden"
-
-
-
-
-        //TABELA DE RANKING
-        // Posição do Rank
-        $(row + ' .rank-number').textContent = '#' + (i + 1)
-        // Level
-        $(row + ' .level').textContent = captalize(round.level)
-        // Pontuação
-        $(row + ' .score').textContent = floatFormat(round.score, 1) + ' pts'
-        // Número de respostas corretas
-        $(row + ' .correct-number').textContent = round.correctNumb
-        // Número de respostas erradas
-        $(row + ' .error-number').textContent = round.errorNumb
-        // Data e Hora
-        $(row + ' .date-hours').textContent = round.data + ' - ' + round.hora
-    })
+            $(row + ' .date-hours').textContent = round.data + ' - ' + round.hora
+        })
+    }
 
     //*** EVENTOS */
     // Botão de sair
